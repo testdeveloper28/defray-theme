9e1f72fff1909a124d2c9f38e4ca6297e41bb8d8
testdeveloper28 <ruchika.sharma@dotsquares.com>
2020-02-26 16:15:07 +0530

    upload feature product

--- Defray_Marketplace/app/code/Dotsquares/Bestseller/registration.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-\Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Dotsquares_Bestseller',
-    __DIR__
-);
--- Defray_Marketplace/app/code/Dotsquares/Blog/registration.php
+++
@@ -1,6 +0,0 @@
-<?php
-\Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Dotsquares_Blog',
-    __DIR__
-);
